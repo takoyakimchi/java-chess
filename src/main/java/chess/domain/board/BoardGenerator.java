ae5170ca70aa48fa5a5a8163760f2b69c254b178
takoyakimchi <c4nyou@gmail.com>
2024-03-31 16:19:45 +0900

    refactor: 함수형 인터페이스 어노테이션 추가

--- src/main/java/chess/domain/board/BoardGenerator.java
+++ src/main/java/chess/domain/board/BoardGenerator.java
@@ -4,6 +4,7 @@ import chess.domain.piece.Piece;
 import chess.domain.position.Position;
 import java.util.Map;
 
+@FunctionalInterface
 public interface BoardGenerator {
 
     Map<Position, Piece> generate();
