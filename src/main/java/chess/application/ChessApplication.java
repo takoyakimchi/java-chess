0bdcb2f375e930347ec9254edd95687d85606e8a
takoyakimchi <c4nyou@gmail.com>
2024-04-03 17:07:53 +0900

    refactor: 컨트롤러 분리

--- src/main/java/chess/application/ChessApplication.java
+++ src/main/java/chess/application/ChessApplication.java
@@ -1,99 +1,14 @@
 package chess.application;
 
+import chess.controller.ChessController;
 import chess.db.GameRepository;
-import chess.domain.board.Board;
-import chess.domain.board.InitialBoardGenerator;
-import chess.domain.game.Game;
-import chess.domain.piece.Color;
-import chess.domain.position.Position;
-import chess.view.Command;
-import chess.view.CommandType;
 import chess.view.InputView;
 import chess.view.OutputView;
-import java.util.Map;
 
 public class ChessApplication {
 
-    private static final InputView INPUT_VIEW = new InputView();
-    private static final OutputView OUTPUT_VIEW = new OutputView();
-    private static final GameRepository GAME_REPOSITORY = new GameRepository();
-
     public static void main(String[] args) {
-        OUTPUT_VIEW.printStartMessage(GAME_REPOSITORY.findGameIds());
-        int gameId = selectGame();
-        Game game = GAME_REPOSITORY.findGameById(gameId);
-        OUTPUT_VIEW.printMessageWhenEnteredRoom(gameId);
-        OUTPUT_VIEW.printBoard(game.getBoard());
-        while (game.isRunning()) {
-            game = play(game, gameId);
-        }
-        OUTPUT_VIEW.printEndMessage();
-        OUTPUT_VIEW.printStatus(game.decideWinStatus());
-    }
-
-    private static int selectGame() {
-        try {
-            Command command = INPUT_VIEW.readCommand();
-            return executeLobbyCommand(command);
-        } catch (UnsupportedOperationException | IllegalArgumentException | IllegalStateException exception) {
-            OUTPUT_VIEW.printErrorMessage(exception.getMessage());
-            return selectGame();
-        }
-    }
-
-    private static int executeLobbyCommand(Command command) {
-        if (command.type() == CommandType.MAKE) {
-            return GAME_REPOSITORY.makeGameThenFindId(Board.generatedBy(new InitialBoardGenerator()), Color.WHITE);
-        }
-        if (command.type() == CommandType.ENTER) {
-            return Integer.parseInt(command.argumentOf(0));
-        }
-        throw new IllegalArgumentException("게임 로비에서 사용할 수 없는 명령어입니다.");
-    }
-
-    private static Game play(Game game, int gameId) {
-        try {
-            Command command = INPUT_VIEW.readCommand();
-            return executeGameCommand(game, gameId, command);
-        } catch (UnsupportedOperationException | IllegalArgumentException | IllegalStateException exception) {
-            OUTPUT_VIEW.printErrorMessage(exception.getMessage());
-            return play(game, gameId);
-        }
-    }
-
-    private static Game executeGameCommand(Game game, int gameId, Command command) {
-        Map<CommandType, GameCommandExecutor> commandExecutionMap = Map.of(
-            CommandType.MOVE, () -> move(game, gameId, command),
-            CommandType.STATUS, () -> showStatus(game),
-            CommandType.END, () -> end(game)
-        );
-        try {
-            return commandExecutionMap.get(command.type()).execute();
-        } catch (NullPointerException exception) {
-            throw new IllegalArgumentException("게임 진행중에 입력할 수 없는 명령어입니다.");
-        }
-    }
-
-    private static Game move(Game game, int gameId, Command command) {
-        Position source = INPUT_VIEW.resolvePosition(command.argumentOf(0));
-        Position target = INPUT_VIEW.resolvePosition(command.argumentOf(1));
-        game = game.moved(source, target);
-        if (game.isRunning()) {
-            GAME_REPOSITORY.saveGame(gameId, game.getBoard(), game.currentTurn());
-        }
-        if (game.isEnd()) {
-            GAME_REPOSITORY.deleteGame(gameId);
-        }
-        OUTPUT_VIEW.printBoard(game.getBoard());
-        return game;
-    }
-
-    private static Game showStatus(Game game) {
-        OUTPUT_VIEW.printStatus(game.decideWinStatus());
-        return game;
-    }
-
-    private static Game end(Game game) {
-        return game.ended();
+        ChessController chessController = new ChessController(new InputView(), new OutputView(), new GameRepository());
+        chessController.run();
     }
 }
---
+++ src/main/java/chess/controller/ChessController.java
@@ -0,0 +1,106 @@
+package chess.controller;
+
+import chess.application.GameCommandExecutor;
+import chess.db.GameRepository;
+import chess.domain.board.Board;
+import chess.domain.board.InitialBoardGenerator;
+import chess.domain.game.Game;
+import chess.domain.piece.Color;
+import chess.domain.position.Position;
+import chess.view.Command;
+import chess.view.CommandType;
+import chess.view.InputView;
+import chess.view.OutputView;
+import java.util.Map;
+
+public class ChessController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final GameRepository gameRepository;
+
+    public ChessController(InputView inputView, OutputView outputView, GameRepository gameRepository) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.gameRepository = gameRepository;
+    }
+
+    public void run() {
+        outputView.printStartMessage(gameRepository.findGameIds());
+        int gameId = selectGame();
+        Game game = gameRepository.findGameById(gameId);
+        outputView.printMessageWhenEnteredRoom(gameId);
+        outputView.printBoard(game.getBoard());
+        while (game.isRunning()) {
+            game = play(game, gameId);
+        }
+        outputView.printEndMessage();
+        outputView.printStatus(game.decideWinStatus());
+    }
+
+    private int selectGame() {
+        try {
+            Command command = inputView.readCommand();
+            return executeLobbyCommand(command);
+        } catch (UnsupportedOperationException | IllegalArgumentException | IllegalStateException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            return selectGame();
+        }
+    }
+
+    private int executeLobbyCommand(Command command) {
+        if (command.type() == CommandType.MAKE) {
+            return gameRepository.makeGameThenFindId(Board.generatedBy(new InitialBoardGenerator()), Color.WHITE);
+        }
+        if (command.type() == CommandType.ENTER) {
+            return Integer.parseInt(command.argumentOf(0));
+        }
+        throw new IllegalArgumentException("게임 로비에서 사용할 수 없는 명령어입니다.");
+    }
+
+    private Game play(Game game, int gameId) {
+        try {
+            Command command = inputView.readCommand();
+            return executeGameCommand(game, gameId, command);
+        } catch (UnsupportedOperationException | IllegalArgumentException | IllegalStateException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            return play(game, gameId);
+        }
+    }
+
+    private Game executeGameCommand(Game game, int gameId, Command command) {
+        Map<CommandType, GameCommandExecutor> commandExecutionMap = Map.of(
+            CommandType.MOVE, () -> move(game, gameId, command),
+            CommandType.STATUS, () -> showStatus(game),
+            CommandType.END, () -> end(game)
+        );
+        try {
+            return commandExecutionMap.get(command.type()).execute();
+        } catch (NullPointerException exception) {
+            throw new IllegalArgumentException("게임 진행중에 입력할 수 없는 명령어입니다.");
+        }
+    }
+
+    private Game move(Game game, int gameId, Command command) {
+        Position source = inputView.resolvePosition(command.argumentOf(0));
+        Position target = inputView.resolvePosition(command.argumentOf(1));
+        game = game.moved(source, target);
+        if (game.isRunning()) {
+            gameRepository.saveGame(gameId, game.getBoard(), game.currentTurn());
+        }
+        if (game.isEnd()) {
+            gameRepository.deleteGame(gameId);
+        }
+        outputView.printBoard(game.getBoard());
+        return game;
+    }
+
+    private Game showStatus(Game game) {
+        outputView.printStatus(game.decideWinStatus());
+        return game;
+    }
+
+    private Game end(Game game) {
+        return game.ended();
+    }
+}
